9bfff28a5e4b5336db7e00d869bac92db7cd06e3
JOCHYMSKI Hugo <jochymski@etu.unista.fr>
2022-03-04 18:18:44 +0100

    Oops les getters

--- Character.java
+++ Character.java
@@ -8,6 +8,12 @@ public abstract class Character
 	protected int wit;
 
 	// Getters
+	public String getName() { return this.name; }
+	public String getRPGClass() { return this.RPGClass; }
+	public int getLife() { return this.life; }
+	public int getAgility() { return this.agility; }
+	public int getStrength() { return this.strength; }
+	public int getWit() { return this.wit; }
 
 	// Methods
 	public void attack(String weapon)
